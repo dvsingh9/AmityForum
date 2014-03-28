0b2d871cb883b8f962450630d7c57d392a98a6b7
dv singh <dvsingh9@gmail.com>
2014-03-28 01:34:01 +0000

    serviceImpl for login service

---
+++ AmityForum/src/main/java/serviceImpl/LoginServiceImpl.java
@@ -0,0 +1,12 @@
+package serviceImpl;
+
+import service.LoginService;
+
+public class LoginServiceImpl implements LoginService{
+
+	public void usingFacebook() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
